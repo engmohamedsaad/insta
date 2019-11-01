b9585152ce5762e5ba56499318d74b9b68bafea4
Eng Mohamed <eng.mohamed.saad4@gmail.com>
2019-11-01 04:11:40 +0200

    connecting to remote mysql server

--- config/database.php
+++ config/database.php
@@ -18,7 +18,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'mysql',
+    // 'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -46,15 +47,34 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        // 'mysql' => [
+        //     'driver' => 'mysql',
+        //     'url' => env('DATABASE_URL'),
+        //     'host' => env('DB_HOST', 'remotemysql.com'),
+        //     'port' => env('DB_PORT', '3306'),
+        //     'database' => env('DB_DATABASE', 'tIOaE5MdbN'),
+        //     'username' => env('DB_USERNAME', 'tIOaE5MdbN'),
+        //     'password' => env('DB_PASSWORD', 'jc87zrXrW7'),
+        //     'unix_socket' => env('DB_SOCKET', ''),
+        //     'charset' => 'utf8mb4',
+        //     'collation' => 'utf8mb4_unicode_ci',
+        //     'prefix' => '',
+        //     'prefix_indexes' => true,
+        //     'strict' => true,
+        //     'engine' => null,
+        //     'options' => extension_loaded('pdo_mysql') ? array_filter([
+        //         PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+        //     ]) : [],
+        // ],
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'remotemysql.com'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'tIOaE5MdbN'),
-            'username' => env('DB_USERNAME', 'tIOaE5MdbN'),
-            'password' => env('DB_PASSWORD', 'jc87zrXrW7'),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => 'remotemysql.com',
+            'port' => '3306',
+            'database' => 'tIOaE5MdbN',
+            'username' => 'tIOaE5MdbN',
+            'password' => 'jc87zrXrW7',
+            'unix_socket' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
